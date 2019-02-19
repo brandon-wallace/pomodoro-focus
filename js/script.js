d2dae10a733a65ab536f748fad6288fc6c315ee8
bwallace <17948072+brandon-wallace@users.noreply.github.com>
2019-02-18 23:05:49 -0500

    Working on timer
    Working on getting the timer to run correctly after the first 25
    minutes.

--- js/script.js
+++ js/script.js
@@ -44,19 +44,25 @@ const decrease = () => {
 
 
 const modalClose = () => {
+    let sessions = document.querySelector("quantity");
     let bg = document.querySelectorAll('input');
     document.querySelector(".modal").style.display = "none";
     if (bg[0].checked === true) {
+        document.querySelector(".clock").style.backgroundColor = 'hsla(0, 0%, 0%, 0.5)';
+        document.querySelector(".info").style.backgroundColor = 'hsla(0, 0%, 0%, 0.5)';
         document.querySelector("article").style.backgroundColor = '#47494B';
         let button = document.querySelectorAll(".btn");
-        for (let i = 0; i < button.length; i++) {
+        for (let i = 0; i < 3; i++) {
             button[i].style.backgroundColor = '#171717';
             button[i].style.border = '4px solid #0E4275';
         }
     } else {
         document.querySelector("article").style.backgroundColor = '#E9EDF2';
         let button = document.querySelectorAll(".btn");
-        for (let i = 0; i < button.length; i++) {
+        document.querySelector(".clock").style.backgroundColor = 'hsla(0, 0%, 100%, 0.47)';
+        document.querySelector(".info").style.backgroundColor = 'hsla(0, 0%, 100%, 0.47)';
+        document.querySelector("article").style.backgroundColor = '#8F8F8F';
+        for (let i = 0; i < 3; i++) {
             button[i].style.backgroundColor = '#FFFFFF';
             button[i].style.border = '4px solid #1E90FF';
         }
@@ -68,6 +74,8 @@ const start = () => {
     running = true;
     let minutes = 25;
     let seconds = 60;
+    let breakminutes = 5;
+    let breakseconds = 60;
     let digits = document.querySelector(".clock-digits");
     beep.play();
 
@@ -82,9 +90,29 @@ const start = () => {
             titleText.innerHTML = `${padZero(minutes)}:${padZero(seconds)} pomodoro focus`;
             if (seconds === 0) {
                 if (seconds === 0 && minutes === 0) {
-                    clearInterval(timerId);
                     timesUpSound.play();
-                    digits.innerHTML = `25:00`;
+                    // digits.innerHTML = `25:00`;
+                    // digits.innerHTML = `5:00`;
+                    digits.style.color = '#FF0909';
+                    document.querySelector(".clock-text").innerHTML = `break!`;
+                    digits.innerHTML = `${padZero(breakminutes)}:${padZero(breakseconds)}`;
+                    titleText.innerHTML = `${padZero(breakminutes)}:${padZero(breakseconds)} pomodoro focus`;
+                    if (breakseconds === 60) {
+                        breakminutes -= 1;
+                    }
+                    breakseconds -= 1;
+                    digits.innerHTML = `${padZero(breakminutes)}:${padZero(breakseconds)}`;
+                    titleText.innerHTML = `${padZero(breakminutes)}:${padZero(breakseconds)} pomodoro focus`;
+                    if (breakseconds === 0) {
+                        if (breakseconds === 0 && breakminutes === 0) {
+                            clearInterval(timerId);
+                        }
+                        breakseconds = 60;
+                    } else {
+                        breakseconds -= 1;
+                        // clearInterval(timerId);
+                    }
+                    clearInterval(timerId);
                 } else {
                     seconds = 60;
                 }
@@ -99,7 +127,9 @@ const start = () => {
 
 const reset = () => {
     running = false;
+    document.querySelector(".clock-text").innerHTML = `work`;
     document.querySelector(".clock-digits").innerHTML = `25:00`;
+    document.querySelector(".clock-digits").style.color = '#0F31DF';
     titleText.innerHTML = `pomodoro focus`
     startBtn.disabled = false;
 }
