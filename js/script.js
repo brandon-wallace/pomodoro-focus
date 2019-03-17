2a929e28f25d1d78b4bbc8c486e6d81bf02b59cd
bwallace <17948072+brandon-wallace@users.noreply.github.com>
2019-03-17 00:49:59 -0400

    Rewrite countdown function
    The countdown function was rewritten from scratch so that there
    is only one function to execute the countdown.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 
 let running = false;
+let sessions = 1;
 let titleText = document.querySelector("title");
 const beep = new Audio('audio/404151_select-01.mp3');
 const timesUpSound = new Audio('audio/216090_bad-beep-incorrect.mp3');
@@ -11,8 +12,9 @@ const startBtn = document.querySelector(".start-btn");
 const padZero = (number) => {
     if (number < 10) {
         return `0${number}`;
+    } else {
+        return `${number}`;
     }
-    return `${number}`;
 }
 
 
@@ -24,7 +26,7 @@ const setUp = () => {
 const increase = () => {
     let interval = Number(document.getElementById("quantity").innerHTML);
     if (interval > 8) {
-        intervar = 9;
+        interval = 9;
     } else {
         interval += 1;
     }
@@ -35,7 +37,7 @@ const increase = () => {
 const decrease = () => {
     let interval = document.getElementById("quantity").innerHTML;
     if (interval < 2) {
-        intervar = 1;
+        interval = 1;
     } else {
         interval -= 1;
     }
@@ -45,10 +47,7 @@ const decrease = () => {
 
 const modalClose = () => {
     let bg = document.querySelectorAll('input');
-    const getSessionCount = () => {
-        let sessions = Number(document.querySelector('.quantity').innerHTML);
-        return sessions;
-    }
+    sessions = Number(document.querySelector('.quantity').innerHTML);
     document.querySelector(".modal").style.display = "none";
     if (bg[0].checked === true) {
         document.querySelector(".clock").style.backgroundColor = 'hsla(0, 0%, 0%, 0.5)';
@@ -70,56 +69,34 @@ const modalClose = () => {
             button[i].style.border = '4px solid #1E90FF';
         }
     }
-    return getSessionCount;
 }
 
-
-const startBreak = () => {
-    let sec = 60;
-    let min = 5;
-    const breakTimer = () => {
-        let breakId = setInterval(() => {
-            if (sec === 60) {
-                min -= 1;
-            }
-            sec -= 1;
-            console.clear();
-            console.log(`min: ${min} sec: ${sec}`);
-            if (sec === 0) {
-                if (sec === 0 && min === 0) {
-                    clearInterval(breakId);
-                } else {
-                    sec = 60;
-                }
-                min -= 1;
-            }
-        }, 1000);
-    }
-    return breakTimer();
-}
-
-
-const startTimer = () => {
-    running = true;
-    return running;
-}
 
 const timerCountdown = () => {
     let sec = 60;
-    let min = 1;
+    let min = 6;
     let digits = document.querySelector(".clock-digits");
+    running = true;
+    console.log(`Starting 25 min interval.`)
     let timerId = setInterval(() => {
-        startTimer();
-        // console.log(getSessionCount);
-
         if (running) {
+            console.log(min);
             if (sec === 60) {
                 min -= 1;
             }
+            if (min > 5) {
+                digits.innerHTML = `${padZero(min - 5)}:${padZero(sec)}`;
+                titleText.innerHTML = `${padZero(min - 5)}:${padZero(sec)} pomodoro focus`;
+            } else {
+                digits.style.color = '#FF0909';
+                document.querySelector(".clock-text").innerHTML = `break!`;
+                document.querySelector(".clock-digits").classList.add('flash');
+                digits.innerHTML = `${padZero(min)}:${padZero(sec)}`;
+                titleText.innerHTML = `${padZero(min)}:${padZero(sec)} pomodoro focus`;
+            }
             sec -= 1;
-            digits.innerHTML = `${padZero(min)}:${padZero(sec)}`;
-            titleText.innerHTML = `${padZero(min)}:${padZero(sec)} pomodoro focus`;
             if (sec === 0) {
+                console.log(sessions);
                 if (sec === 0 && min === 0) {
                     // timesUpSound.play();
                     running = false;
@@ -135,83 +112,6 @@ const timerCountdown = () => {
         }
     }, 1000);
 }
-
-// const startBreak = () => {
-//     let min = 1;
-//     let sec = 60;
-//     let digits = document.querySelector(".clock-digits");
-//     let breakId = setInterval(() => {
-//         if (running) {
-//             if (sec === 60) {
-//                 min -= 1;
-//                 console.log(sessions);
-//             }
-//             sec -= 1;
-//             digits.innerHTML = `${padZero(min)}:${padZero(sec)}`;
-//             titleText.innerHTML = `${padZero(min)}:${padZero(sec)} pomodoro focus`;
-//             if (sec === 0) {
-//                 if (sec === 0 && min === 0) {
-//                     timesUpSound.play();
-//                     digits.style.color = '#0F31DF';
-//                     digits.innerHTML = `25:00`;
-//                     document.querySelector(".clock-text").innerHTML = `work`;
-//                     sessions -= 1;
-//                     if (sessions === 0) {
-//                         clearInterval(breakId);
-//                     } else {
-//                         startTimer();
-//                     }
-//                 } else {
-//                     sec = 60;
-//                 }
-//                 min -= 1;
-//             }
-//         } else {
-//             clearInterval(breakId);
-//         }
-//     }, 1000);
-// }
-
-
-// const startTimer = () => {
-//     running = true;
-//     let minutes = 1;
-//     let seconds = 60;
-//     let digits = document.querySelector(".clock-digits");
-//     beep.play();
-//
-//     let timerId = setInterval(() => {
-//         if (running) {
-//             startBtn.innerHTML = 'PAUSE';
-//             startBtn.disabled = true;
-//             if (seconds === 60) {
-//                 minutes -= 1;
-//             }
-//             seconds -= 1;
-//             digits.innerHTML = `${padZero(minutes)}:${padZero(seconds)}`;
-//             titleText.innerHTML = `${padZero(minutes)}:${padZero(seconds)} pomodoro focus`;
-//             if (seconds === 0) {
-//                 if (seconds === 0 && minutes === 0) {
-//                     timesUpSound.play();
-//                     digits.innerHTML = `05:00`;
-//                     digits.style.color = '#FF0909';
-//                     document.querySelector(".clock-text").innerHTML = `break!`;
-//                     document.querySelector(".clock-digits").classList.add('flash');
-//                     startBreak();
-//                     if (sessions === 0) {
-//                       clearInterval(breakId);
-//                     }
-//                     clearInterval(timerId);
-//                 } else {
-//                     seconds = 60;
-//                 }
-//                 minutes -= 1;
-//             }
-//         } else {
-//             clearInterval(timerId);
-//         }
-//     }, 1000)
-// }
 
 
 const reset = () => {
