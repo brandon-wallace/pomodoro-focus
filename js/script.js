d837447a56b5aebcfc11cad4be2f5d59aaee3ea9
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-03-07 00:35:15 -0500

    Add event listener

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 "use strict";
 
+const submitBttn = document.querySelector('.submit-btn button');
+
+const closeModal = () => {
+    console.log(`Closed modal`);
+    document.querySelector(".modal").style.display = "none";
+}
+
+submitBttn.addEventListener('click', closeModal); 
 
 let running = false;
 let sessions = 1;
@@ -7,43 +15,6 @@ let titleText = document.querySelector("title");
 const startBtn = document.querySelector(".start-btn");
 const beep = new Audio('audio/404151_select-01.mp3');
 const timesUpSound = new Audio('audio/216090_bad-beep-incorrect.mp3');
-
-const openButton = document.querySelector('.settings-bttn');
-const closeButton = document.querySelector('.close-modal');
-const increaseButtons = document.querySelectorAll('.increase-time');
-const decreaseButtons = document.querySelectorAll('.decrease-time');
-const increaseBreakButtons = document.querySelectorAll('.increase-break');
-const decreaseBreakButtons = document.querySelectorAll('.decrease-break');
-
-
-const addMinute = () => {
-    let minute = parseInt(document.querySelector('.time').value, 10);
-    if (minute === 60) return;
-    minute += 1;
-    document.querySelector('.time').value = minute;
-}
-
-
-const subtractMinute = () => {
-    let minute = document.querySelector('.time');
-    if (minute.value <= 1) return;
-    minute.value -= 1;
-}
-
-
-const addBreakMinute = () => {
-    let minute = parseInt(document.querySelector('.break').value, 10);
-    if (minute === 60) return;
-    minute += 1;
-    document.querySelector('.break').value = minute;
-}
-
-
-const subtractBreakMinute = () => {
-    let minute = document.querySelector('.break');
-    if (minute.value <= 1) return;
-    minute.value -= 1;
-}
 
 
 const padZero = (number) => {
@@ -55,8 +26,7 @@ const padZero = (number) => {
 }
 
 
-    /*
-const addMinute = () => {
+const increase = () => {
     let interval = Number(document.getElementById("quantity").innerHTML);
     if (interval > 8) {
         interval = 9;
@@ -65,10 +35,9 @@ const addMinute = () => {
     }
     document.getElementById("quantity").innerHTML = interval;
 }
-    */
 
-    /*
-const subtractTime = () => {
+
+const decrease = () => {
     let interval = document.getElementById("quantity").innerHTML;
     if (interval < 2) {
         interval = 1;
@@ -77,20 +46,17 @@ const subtractTime = () => {
     }
     document.getElementById('quantity').innerHTML = interval;
 }
-    */
 
 
-const openModal = () => {
-    console.log(`Opening modal`);
+const setOptions = () => {
     document.querySelector(".modal").style.display = "flex";
 }
 
 
-const closeModal = () => {
-    //let bg = document.querySelectorAll('input');
-    //sessions = Number(document.querySelector('.quantity').innerHTML);
+const modalClose = () => {
+    let bg = document.querySelectorAll('input');
+    sessions = Number(document.querySelector('.quantity').innerHTML);
     document.querySelector(".modal").style.display = "none";
-    /*
     if (bg[0].checked === true) {
         document.querySelector(".clock").style.backgroundColor = 'hsla(0, 0%, 0%, 0.5)';
         document.querySelector(".info").style.backgroundColor = 'hsla(0, 0%, 0%, 0.5)';
@@ -114,7 +80,6 @@ const closeModal = () => {
             button[i].style.border = '4px solid #193FFF';
         }
     }
-    */
 }
 
 
@@ -173,23 +138,3 @@ const reset = () => {
     titleText.innerHTML = `pomodoro focus`;
     startBtn.disabled = false;
 }
-
-
-for (let a = 0; a < increaseButtons.length; a++) {
-    increaseButtons[a].addEventListener('click', addMinute, false);
-}
-
-for (let b = 0; b < decreaseButtons.length; b++) {
-    decreaseButtons[b].addEventListener('click', subtractMinute, false);
-}
-
-for (let a = 0; a < increaseBreakButtons.length; a++) {
-    increaseBreakButtons[a].addEventListener('click', addBreakMinute, false);
-}
-
-for (let b = 0; b < decreaseBreakButtons.length; b++) {
-    decreaseBreakButtons[b].addEventListener('click', subtractBreakMinute, false);
-}
-
-openButton.addEventListener('click', openModal);
-closeButton.addEventListener('click', closeModal);
