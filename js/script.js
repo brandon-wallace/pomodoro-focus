b97483e300fd0b45bd2b04d5b02c579a5f569f53
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-04-04 23:05:56 -0400

    Remove excess code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 "use strict";
 
 let running = false;
-let loop = false;
-//const seconds = 60;
 let titleText = document.querySelector('title');
 //const beepSound = new Audio('audio/404151_select-01.mp3');
 const resetBttn = document.querySelector('.reset-bttn');
@@ -12,7 +10,7 @@ const submitBttn = document.querySelector('.submit__bttn button');
 const addBttn = document.querySelectorAll('.add');
 const subtractBttn = document.querySelectorAll('.subtract');
 //const timesUpSound = new Audio('audio/216090_bad-beep-incorrect.mp3');
-let workTime = 25;
+let workTime = 1;
 let breakShort = 5;
 let breakLong = 15;
 
@@ -26,8 +24,18 @@ const padZero = (number) => {
 }
 
 
-const startCountdown = (minutes) => {
+const startTimer = (minutes, timerType) => {
     let seconds = 60;
+    if (timerType === 'work') {
+        minutes = 1;
+        document.querySelector('.timer__text').textContent = `WORK`;
+    } else if (timerType === 'break') {
+        minutes = 5;
+        document.querySelector('.timer__text').textContent = `BREAK`;
+    } else if (timerType === 'longbreak') {
+        minutes = 15;
+        document.querySelector('.timer__text').textContent = `BREAK`;
+    }
     let timeId = setInterval(() => {
         if (seconds === 60) {
             minutes -= 1;
@@ -38,6 +46,7 @@ const startCountdown = (minutes) => {
         if (seconds === 0) {
             if (minutes === 0) {
                 clearInterval(timeId);
+                //callback();
             }
             seconds = 60;
         }
@@ -45,7 +54,7 @@ const startCountdown = (minutes) => {
 }
 
 
-const startBreak = (breakTime) => {
+const startBreak = (breaktime) => {
     let seconds = 60;
     let breakId = setInterval(() => {
         if (seconds === 60) {
@@ -53,7 +62,7 @@ const startBreak = (breakTime) => {
         }
         seconds -= 1;
         document.querySelector('.timer__text').textContent = `BREAK`;
-        console.log(`Break: ${min} ${sec}`);
+        console.log(`Break: ${breaktime} ${seconds}`);
         if (seconds === 0) {
             if (breakTime === 0) {
                 clearInterval(breakId);
@@ -63,33 +72,8 @@ const startBreak = (breakTime) => {
 }
 
 
-const main = (workTime, breakShort, breakLong) => {
-    /*
-    console.log(`Work: ${workTime}`);
-    console.log(`Short Break: ${breakShort}`);
-    console.log(`Long Break: ${breakLong}`);
-    console.log(`\n\n`);
-    */
-    for (let count = 3; count > 0; count--) {
-        //countdown();
-        console.log(count);
-        console.log(`Work time: ${workTime}`);
-        if (count < 2) {
-            breakShort = breakLong;
-        }
-        console.log(`Break time: ${breakShort}`);
-    }
-    console.log('END!');
-}
-
-
-// RESET TIMER
-const resetTimer = () => {
-    running = false;
-    document.querySelector('.timer__text').textContent = `WORK`;
-    document.querySelector('.timer__digits').textContent = `25:00`;
-    titleText.innerHTML = `pomodoro focus`;
-    startBttn.disabled = false;
+const main = (callback) => {
+    //startTimer(workTime, startBreak(breakShort));
 }
 
 
@@ -105,13 +89,20 @@ for (let item of controls) {
             resetTimer();
         }
         if (event.currentTarget.classList[2] === 'start-bttn') {
-            //startTimer();
-            //countdown(startTime, seconds);
-            //main(workTime, breakShort, breakLong);
-            startCountdown(workTime);
+            startTimer(1, 'work');
         }
     });
 }    
+
+
+// RESET TIMER
+const resetTimer = () => {
+    running = false;
+    document.querySelector('.timer__text').textContent = `WORK`;
+    document.querySelector('.timer__digits').textContent = `25:00`;
+    titleText.innerHTML = `pomodoro focus`;
+    startBttn.disabled = false;
+}
 
 
 // CLOSE
@@ -125,13 +116,6 @@ const closeModal = () => {
     workTime = Number(document.querySelector('.time').textContent);
     breakShort = Number(document.querySelector('.break-short').textContent);
     breakLong = Number(document.querySelector('.break-long').textContent);
-    //let time = Number(document.querySelector('.time').textContent);
-    //workTime = time;
-    //let shortbreak = Number(document.querySelector('.break-short').textContent);
-    //shortBreak = shortbreak;
-    //let longbreak = Number(document.querySelector('.break-long').textContent);
-    //longBreak = longbreak;
-    //loop = document.querySelector('.loop');
     document.querySelector('.modal').style.display = 'none';
     document.querySelector('.timer__digits').textContent = `${padZero(workTime)}:00`;
 }
@@ -158,60 +142,3 @@ for (let elem of subtractBttn) {
         event.currentTarget.previousElementSibling.textContent = digit;
     });
 }
-
-/*
-const longBreak = (breakTime) => {
-    let seconds = 60;
-    let breakId = setInterval(() => {
-        if (seconds === 60) {
-            breatTime -= 1;
-        }
-        seconds -= 1;
-        console.log(`Break: ${min} ${sec}`);
-    }, 1000);
-}
-*/
-
-
-// START TIMER
-/*
-const startTimer = (longtime, shorttime, breaktime) => {
-    //let count = 3;
-    //startBttn.blur();
-    //beepSound.play();
-    let min = startTime;
-    let sec = 60;
-    running = true;
-    let digits = document.querySelector('.timer__digits');
-    let timeId = setInterval(() => {
-        if (running) {
-            startBttn.disabled = true;
-            if (sec === 60) {
-                min -= 1;
-            }
-            sec -= 1;
-            console.log(`${min} ${sec}`);
-            digits.textContent = `${padZero(min)}:${padZero(sec)}`;
-            titleText.textContent = `${padZero(min)}:${padZero(sec)} pomodoro focus`;
-            if (sec === 0) {
-                if (min === 0) {
-                    min = min + shortBreak;
-                    document.querySelector(".timer__text").textContent = `BREAK`;
-                    //beepSound.play();
-                    if (min === 0 && sec === 0) {
-                        //timesUpSound.play();
-                        if (count === 0) {
-                            shortBreak = 0;
-                            min = longBreak;
-                            longBreak = 0;
-                            clearInterval(timeId);
-                        }
-                        count -= 1;
-                    }
-                }
-                sec = 60;
-            }
-        } 
-    }, 1000);
-};
-*/
